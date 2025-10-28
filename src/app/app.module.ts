99912fd282563e94d0d5aacab006e9fa6ae23354
Luis Meza <luimi@live.com>
2025-10-28 18:18:05 -0500

    update translate y swiper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,28 +9,48 @@ import { AppComponent } from './app.component';
 import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AppConfig } from './utils/appConfig';
 import { UtilsService } from './utils/utils.service';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { provideTranslateHttpLoader, TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule, TranslateLoader, provideTranslateService } from '@ngx-translate/core';
 import { AdvertiseComponent } from './components/advertise/advertise.component';
 
 export function LanguageLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
+    //return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
+    return new TranslateHttpLoader();
 }
 
-@NgModule({ declarations: [AppComponent, AdvertiseComponent],
+@NgModule({
+    declarations: [AppComponent, AdvertiseComponent],
     bootstrap: [AppComponent],
     schemas: [],
-    exports: [AdvertiseComponent], imports: [BrowserModule, IonicModule.forRoot({ mode: 'ios', hardwareBackButton: false, swipeBackEnabled: false }), AppRoutingModule,
-        TranslateModule.forRoot({
+    exports: [AdvertiseComponent], 
+    imports: [
+        BrowserModule, 
+        IonicModule.forRoot({ mode: 'ios', hardwareBackButton: false, swipeBackEnabled: false }), 
+        AppRoutingModule,
+        /*TranslateModule.forRoot({
             loader: {
                 provide: TranslateLoader,
                 useFactory: (LanguageLoader),
                 deps: [HttpClient]
             }
-        })], providers: [{
+        })*/
+    ],
+    providers: [
+        {
             provide: APP_INITIALIZER,
             useFactory: AppConfig,
             deps: [UtilsService],
             multi: true
-        }, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideHttpClient(withInterceptorsFromDi())] })
-export class AppModule {}
+        }, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+        provideHttpClient(withInterceptorsFromDi()),
+        provideTranslateService({
+            lang: 'en',
+            fallbackLang: 'en',
+            loader: provideTranslateHttpLoader({
+                prefix: '/assets/i18n/',
+                suffix: '.json'
+            })
+        }),
+    ]
+})
+export class AppModule { }
