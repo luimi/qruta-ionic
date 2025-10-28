1712d663a3e68ef7fa66ed544387771d8ea4d9ce
Luis Meza <luimi@live.com>
2025-10-28 15:40:27 -0500

    update angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AppConfig } from './utils/appConfig';
 import { UtilsService } from './utils/utils.service';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
@@ -17,24 +17,20 @@ export function LanguageLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
 }
 
-@NgModule({
-  declarations: [AppComponent, AdvertiseComponent],
-  imports: [BrowserModule, IonicModule.forRoot({mode: 'ios', hardwareBackButton: false, swipeBackEnabled: false}), AppRoutingModule, HttpClientModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: (LanguageLoader),
-        deps: [HttpClient]
-      }
-    })],
-  providers: [{
-    provide: APP_INITIALIZER,
-    useFactory: AppConfig,
-    deps: [UtilsService],
-    multi: true
-   },{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
-  schemas: [],
-  exports: [AdvertiseComponent]
-})
+@NgModule({ declarations: [AppComponent, AdvertiseComponent],
+    bootstrap: [AppComponent],
+    schemas: [],
+    exports: [AdvertiseComponent], imports: [BrowserModule, IonicModule.forRoot({ mode: 'ios', hardwareBackButton: false, swipeBackEnabled: false }), AppRoutingModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: (LanguageLoader),
+                deps: [HttpClient]
+            }
+        })], providers: [{
+            provide: APP_INITIALIZER,
+            useFactory: AppConfig,
+            deps: [UtilsService],
+            multi: true
+        }, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
